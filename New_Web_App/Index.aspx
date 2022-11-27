5c2d832f950a57b445ff067881713a5fc20d636d
togrul-asgerli <81424132+togrul-asgerli@users.noreply.github.com>
2022-11-27 20:39:06 +0400

    Add files via upload

---
+++ New_Web_App/Index.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="New_Web_App.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <link href="cssdesign.css" rel="stylesheet" type="text/css"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="lbl_name" runat="server" Text="CategoryName"></asp:Label>
+
+            <asp:TextBox ID="txt_input" runat="server" CssClass=".txtstyle" style="margin-top: 29px" Width="160px"></asp:TextBox>
+            <asp:Button ID="btn_search" runat="server" OnClick="btn_search_Click" style="margin-left: 42px" Text="Search" CssClass=".btn_search"/>
+
+        </div>
+        <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:GridView ID="datagrid" runat="server">
+            </asp:GridView>
+        </p>
+        <asp:Label ID="lbl_mes" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ New_Web_App/Index.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace New_Web_App
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-SQ9BA76\\SQLEXPRESS;Integrated Security=True; Database=Northwind;");
+        SqlCommand cmd;
+        DataTable dt=new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_search_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand("Search", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name",txt_input.Text);
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                Direction = ParameterDirection.Output,
+                ParameterName = "@result",
+                SqlDbType = SqlDbType.Int
+            });
+            cmd.ExecuteNonQuery();
+            if(cmd.Parameters["@result"].Value.ToString()=="1")
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(dt);
+                datagrid.DataSource = dt;
+                datagrid.DataBind();
+                lbl_mes.Text = "";
+            }
+            else
+            {
+                
+                lbl_mes.Text = "Data not found!";
+                datagrid.DataSource = null;
+                datagrid.DataBind();
+            }
+            con.Close();
+        }
+
+        protected void grd_result_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
